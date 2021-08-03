3912320ce619fc418d7f5b3d9b06ea30f6debed3
YaghoMattos <mattosyagho@gmail.com>
2021-08-03 18:00:57 -0300

    feature: update app.js, screen and server

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import Parse from 'parse/react-native.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { Chat } from './src/screens/Chat';
+import { LoginPatient } from './src/components/PatientLogin';
+import { Home } from './src/screens/Home';
+import { Monitoring } from './src/screens/Monitoring';
+import { ListSelectExercises } from './src/screens/ListSelectExercises';
+
+Parse.setAsyncStorage(AsyncStorage);
+Parse.serverURL = 'https://reabilitacao.b4a.io/';
+Parse.initialize(
+  'm0GlXlzavxfoYkdEfQOfcvg3P57xCrcQDhTwJ0O8',
+  'fKv7jVaceov8sgadLXeGa6HFpPS3UrUOrpCyAhjw'
+);
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="LoginPatient" component={LoginPatient} />
+        <Stack.Screen name="Chat" component={Chat} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ListSelectExercises" component={ListSelectExercises} />
+        <Stack.Screen name="Monitoring" component={Monitoring} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
