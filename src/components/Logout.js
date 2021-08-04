515293e4419cda76fa1a3d80f6153cef8d97d1a3
YaghoMattos <mattosyagho@gmail.com>
2021-08-04 00:40:00 -0300

    feature: logout component

---
+++ src/components/Logout.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Alert, Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { StackActions } from '@react-navigation/native';
+
+import Parse from 'parse/react-native';
+
+import Styles from './Styles';
+
+export const Logout = () => {
+  const navigation = useNavigation();
+
+  const userLogout = async function () {
+    let currentUser = await Parse.User.currentAsync();
+    await Parse.User.logOut()
+      .then(async () => {
+        currentUser = await Parse.User.currentAsync();
+        if (currentUser === null) {
+          Alert.alert('Success!', 'No patient is logged in anymore!');
+        }
+        navigation.dispatch(StackActions.popToTop());
+        return true;
+      })
+      .catch((error) => {
+        Alert.alert('Error in logout!', error.message);
+        navigation.dispatch(StackActions.popToTop());
+        return false;
+      });
+  };
+
+  return (
+    <View style={Styles.login_wrapper}>
+      <View style={Styles.form}>
+        <TouchableOpacity onPress={() => userLogout()}>
+          <View style={Styles.button}>
+            <Text style={Styles.button_label}>{'Sair'}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
