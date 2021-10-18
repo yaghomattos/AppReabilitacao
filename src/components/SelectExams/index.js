50af2013860e3f4b8fb4b314667cb21030e8f407
YaghoMattos <mattosyagho@gmail.com>
2021-10-18 10:53:40 -0300

    feature: select exams component

---
+++ src/components/SelectExams/index.js
@@ -0,0 +1,51 @@
+import Parse from 'parse/react-native.js';
+
+export async function readSelectExams(patientId, examId) {
+  var currentUser = {
+    __type: 'Pointer',
+    className: 'Patient',
+    objectId: patientId,
+  };
+  var currentExam = {
+    __type: 'Pointer',
+    className: 'Exam',
+    objectId: examId,
+  };
+
+  const SelectExams = Parse.Object.extend('SelectExams');
+  const query = new Parse.Query(SelectExams);
+  query.equalTo('patient', currentUser);
+  query.equalTo('exam', currentExam);
+  try {
+    const results = await query.find();
+    if (results != undefined) return results[0].id;
+    return false;
+  } catch (error) {
+    console.error('Error while fetching SelectExams', error);
+    return false;
+  }
+}
+
+export async function updateSelectExams(id, formId) {
+  var formPointer = {
+    __type: 'Pointer',
+    className: 'Form',
+    objectId: formId,
+  };
+
+  const SelectExams = Parse.Object.extend('SelectExams');
+  const query = new Parse.Query(SelectExams);
+  try {
+    const object = await query.get(id);
+    if (object.get('check') != true) object.set('check', true);
+    if (object.get('form') == undefined || object.get('form') == '')
+      object.set('form', formPointer);
+    try {
+      const response = await object.save();
+    } catch (error) {
+      console.error('Error while updating SelectExams', error);
+    }
+  } catch (error) {
+    console.error('Error while retrieving object SelectExams', error);
+  }
+}
