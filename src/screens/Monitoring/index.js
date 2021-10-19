8a67f05538bbfef415b89bec3d8be9e67bfe13c9
YaghoMattos <mattosyagho@gmail.com>
2021-10-19 11:30:56 -0300

    feature: add exam in monitoring

--- src/screens/Monitoring/index.js
+++ src/screens/Monitoring/index.js
@@ -6,22 +6,29 @@ import {
   View,
   FlatList,
   TouchableHighlight,
+  ScrollView,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useParseQuery } from '@parse/react-native';
 import Parse from 'parse/react-native.js';
 
 import DateTimePicker from '@react-native-community/datetimepicker';
-import { Ionicons } from '@expo/vector-icons';
 import { List, Divider } from 'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
 
 import styles from './styles';
 
+var date = new Date();
+
+const parseQueryExams = new Parse.Query('SelectExams');
+parseQueryExams.ascending('createdAt');
 const parseQuery = new Parse.Query('SelectExercises');
 parseQuery.ascending('createdAt');
 
 var exercise = '';
 var totalExercise = 0;
+var exam = '';
+var totalExam = 0;
 
 async function Search(patientId) {
   var patientPointer = {
@@ -31,8 +38,8 @@ async function Search(patientId) {
   };
 
   parseQuery.equalTo('patient', patientPointer);
-  var results = await parseQuery.find();
-  totalExercise = results.length;
+  var resultPatient = await parseQuery.find();
+  totalExercise = resultPatient.length;
 
   const query = new Parse.Query('SelectExercises');
   query.ascending('createdAt');
@@ -40,6 +47,17 @@ async function Search(patientId) {
 
   var result = await query.find();
   exercise = result;
+
+  parseQueryExams.equalTo('patient', patientPointer);
+  var resultPatientExam = await parseQueryExams.find();
+  totalExam = resultPatientExam.length;
+
+  const queryExam = new Parse.Query('SelectExams');
+  queryExam.ascending('createdAt');
+  queryExam.equalTo('check', true);
+
+  var resultExam = await queryExam.find();
+  exam = resultExam;
 }
 
 function CaseBad() {
@@ -61,13 +79,13 @@ function Productivy(props) {
   else return <CaseGreat />;
 }
 
-function CurrentDate() {
-  var date = new Date().getDate();
-  var month = new Date().getMonth();
-  var year = new Date().getFullYear();
+function getCurrentDate(data) {
+  var date = data.getDate();
+  var month = data.getMonth();
+  var year = data.getFullYear();
 
-  var monName;
-  monName = new Array(
+  var monthName;
+  monthName = new Array(
     'janeiro',
     'fevereiro',
     'março',
@@ -81,98 +99,141 @@ function CurrentDate() {
     'novembro',
     'dezembro'
   );
-
-  return date + ' de ' + monName[month] + ', ' + year;
+  return date + ' de ' + monthName[month] + ', ' + year;
 }
+
+const displayDate = () => {
+  return <Text style={styles.date}>{getCurrentDate(date)}</Text>;
+};
 
 export function Monitoring(props) {
   const navigation = useNavigation();
 
   const patientId = props.route.params;
-  const currentDate = new Date();
 
-  const [date, setDate] = useState(new Date());
+  const lastDate = new Date();
+
   const [show, setShow] = useState(false);
 
   const results = useParseQuery(parseQuery).results;
+  const resultsExam = useParseQuery(parseQueryExams).results;
   Parse.User._clearCache();
 
   Search(patientId);
 
   return (
-    <>
+    <SafeAreaView style={styles.container}>
       <StatusBar />
-      <SafeAreaView style={styles.container}>
-        <View style={styles.header}>
-          <Ionicons
-            name="arrow-back"
-            size={24}
-            style={styles.back}
-            onPress={() => navigation.goBack()}
-          />
-          <TouchableHighlight
-            style={styles.highlight}
-            activeOpacity={0}
-            onPress={() => {
-              setShow(true);
+      <View style={styles.header}>
+        <Ionicons
+          name="arrow-back"
+          size={24}
+          style={styles.back}
+          onPress={() => navigation.goBack()}
+        />
+        <TouchableHighlight
+          style={styles.highlight}
+          activeOpacity={0}
+          onPress={() => {
+            setShow(true);
+          }}
+        >
+          {displayDate()}
+        </TouchableHighlight>
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode="date"
+            display="calendar"
+            maximumDate={lastDate}
+            onChange={(dateString) => {
+              const newDate = new Date(dateString.nativeEvent.timestamp);
+              date = newDate;
+              setShow(false);
             }}
-          >
-            <Text style={styles.date}>{CurrentDate()}</Text>
-          </TouchableHighlight>
-          {show && (
-            <DateTimePicker
-              testID="dateTimePicker"
-              value={new Date(date)}
-              mode="date"
-              display="calendar"
-              maximumDate={currentDate}
-              onChange={(date) => {
-                setDate(date);
-              }}
-            />
-          )}
-        </View>
-        <View style={styles.today}>
-          <Text style={styles.title}>{'Feito Hoje'}</Text>
-        </View>
-        <Divider style={styles.divider} />
-        <View style={styles.exerciseBox}>
-          <Text style={styles.subTitle}>{'Exercícios:'}</Text>
-          <View style={styles.exerciseContainer}>
-            <FlatList
-              data={exercise}
-              keyExtractor={(item) => item.id}
-              ItemSeparatorComponent={() => <Divider />}
-              renderItem={({ item }) => (
-                <List.Item
-                  style={styles.item}
-                  title={item.get('exercise').get('name')}
-                  titleNumberOfLines={1}
-                  titleStyle={styles.description}
-                  description={item.get('exercise').get('description')}
-                  descriptionNumberOfLines={5}
-                  descriptionStyle={styles.description}
-                />
-              )}
-            />
-          </View>
-          <View>
-            <Text style={styles.subTitle}>{'Quantidade:'}</Text>
-            <View>
-              <Text style={styles.feedback}>
-                {' '}
-                {exercise.length + ` de ${totalExercise} exercícios concluídos`}
-              </Text>
+          />
+        )}
+      </View>
+      <View style={styles.today}>
+        <Text style={styles.title}>{'Feito Hoje'}</Text>
+      </View>
+      <Divider style={styles.divider} />
+      <ScrollView horizontal={false}>
+        <ScrollView horizontal={true}>
+          <View style={styles.exerciseBox}>
+            <Text style={styles.subTitle}>{'Exercícios:'}</Text>
+            <View style={styles.exerciseContainer}>
+              <FlatList
+                nestedScrollEnabled={true}
+                data={exercise}
+                keyExtractor={(item) => item.id}
+                ItemSeparatorComponent={() => <Divider />}
+                renderItem={({ item }) => (
+                  <List.Item
+                    title={item.get('exercise').get('name')}
+                    titleNumberOfLines={1}
+                    titleStyle={styles.description}
+                    description={item.get('exercise').get('description')}
+                    descriptionNumberOfLines={5}
+                    descriptionStyle={styles.description}
+                  />
+                )}
+              />
             </View>
-            <Text style={styles.subTitle}>{'Produtividade:'}</Text>
             <View>
-              <Text style={styles.feedback}>
-                {Productivy(exercise.length / totalExercise)}
-              </Text>
+              <Text style={styles.subTitle}>{'Quantidade:'}</Text>
+              <View>
+                <Text style={styles.feedback}>
+                  {' '}
+                  {exercise.length +
+                    ` de ${totalExercise} exercícios concluídos`}
+                </Text>
+              </View>
+              <Text style={styles.subTitle}>{'Produtividade:'}</Text>
+              <View>
+                <Text style={styles.feedback}>
+                  {Productivy(exercise.length / totalExercise)}
+                </Text>
+              </View>
+            </View>
+            <Text style={styles.subTitle}>{'Exames:'}</Text>
+            <View style={styles.exerciseContainer}>
+              <FlatList
+                nestedScrollEnabled
+                data={exam}
+                keyExtractor={(item) => item.id}
+                ItemSeparatorComponent={() => <Divider />}
+                renderItem={({ item }) => (
+                  <List.Item
+                    title={item.get('exam').get('name')}
+                    titleNumberOfLines={1}
+                    titleStyle={styles.description}
+                    description={item.get('exam').get('description')}
+                    descriptionNumberOfLines={5}
+                    descriptionStyle={styles.description}
+                  />
+                )}
+              />
+            </View>
+            <View>
+              <Text style={styles.subTitle}>{'Quantidade:'}</Text>
+              <View>
+                <Text style={styles.feedback}>
+                  {' '}
+                  {exam.length + ` de ${totalExam} exames concluídos`}
+                </Text>
+              </View>
+              <Text style={styles.subTitle}>{'Produtividade:'}</Text>
+              <View>
+                <Text style={styles.feedback}>
+                  {Productivy(exam.length / totalExam)}
+                </Text>
+              </View>
             </View>
           </View>
-        </View>
-      </SafeAreaView>
-    </>
+        </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
