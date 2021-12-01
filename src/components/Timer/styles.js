75679b5f05bafb43f8dddced09e7f1b3d8e22e2a
YaghoMattos <mattosyagho@gmail.com>
2021-12-01 10:23:52 -0300

    styles: timer

---
+++ src/components/Timer/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3E9ACD',
+  },
+
+  title: {
+    color: '#fff',
+    fontSize: 15,
+  },
+  buttons: {
+    width: '40%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+});
+
+export default styles;
