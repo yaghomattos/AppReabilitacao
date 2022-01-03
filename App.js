7c0755d00f40da45ae1186a1f62eab0eead9eed3
Yagho Mattos <mattosyagho@gmail.com>
2022-01-03 12:32:39 -0300

    feature: connect to firebase services

--- App.js
+++ App.js
@@ -1,18 +1,7 @@
-import 'react-native-gesture-handler';
 import React from 'react';
-
-import Parse from 'parse/react-native.js';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import 'react-native-gesture-handler';
 import { AppRoutes } from './src/routes/app.routes';
-
-Parse.initialize(
-  'DlDuXTrOQAoLKh6uXuibPpbADVFTwA3j97lEZj4o',
-  'TlnnvFvI6d6LCmF0gAMlRIMPnSxuvrZUOhhockxz'
-);
-Parse.serverURL = 'https://reabilitation.b4a.io/';
-Parse.setAsyncStorage(AsyncStorage);
-Parse.enableLocalDatastore();
+import './src/services/firebase';
 
 const App = () => {
   return <AppRoutes />;
---
+++ src/services/firebase.js
@@ -0,0 +1,37 @@
+import {
+  REACT_APP_API_KEY,
+  REACT_APP_APP_ID,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_MEASUREMENT_ID,
+  REACT_APP_MENSSAGING_SENDER_ID,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+} from '@env';
+import firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MENSSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+const auth = firebase.auth();
+const database = firebase.database();
+const storage = firebase.storage();
+
+export { firebase, database, auth, storage };
