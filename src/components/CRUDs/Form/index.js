fb2aa13c1b24f18e54d0be9a97bc01c4fa6ef844
Yagho Mattos <mattosyagho@gmail.com>
2022-01-03 15:08:32 -0300

    feature: firebase crud form

--- src/components/CRUDs/Form/index.js
+++ src/components/CRUDs/Form/index.js
@@ -1,34 +1,48 @@
-import Parse from 'parse/react-native.js';
+import { database } from '../../../services/firebase';
 
-export async function createForm(props) {
-  const myNewObject = new Parse.Object('Form');
-  myNewObject.set('Frequency', props.frequency);
-  myNewObject.set('Saturation', props.saturation);
-  myNewObject.set('Dyspnea', props.dyspnea);
-  myNewObject.set('Fatique', props.fatigue);
-  if (props.reps) myNewObject.set('Reps', props.reps);
-  try {
-    const result = await myNewObject.save();
-    if (result !== undefined) return result.id;
-    return false;
-  } catch (error) {
-    console.error('Error while creating Form: ', error);
-    return false;
-  }
+export async function createPostForm(props) {
+  const formRef = database.ref('form');
+
+  formRef
+    .push({
+      reps: '',
+      time: '',
+      frequency: '',
+      saturation: '',
+      dyspnea: '',
+      fatique: '',
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    })
+    .then(() => {
+      console.log('Formulário cadastrado');
+    });
 }
 
-export async function readForm() {
-  const Form = Parse.Object.extend('Form');
-  const query = new Parse.Query(Form);
-  query.equalTo('objectId', 'xKue915KBG');
-  try {
-    const results = await query.find();
-    if (results[0].id !== undefined) {
-      return results[0];
-    }
+export async function createPreForm(props) {
+  const formRef = database.ref('form');
+
+  formRef
+    .push({
+      frequency: '',
+      saturation: '',
+      dyspnea: '',
+      fatique: '',
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    })
+    .then(() => {
+      console.log('Formulário cadastrado');
+    });
+}
+
+export async function readForm(props) {
+  const formRef = await database.ref(`form/${props}`).get();
+
+  if (!formRef.exists()) {
+    console.log('Form does not exists.');
     return false;
-  } catch (error) {
-    console.error('Error while fetching Form', error);
-    return false;
+  } else {
+    return formRef;
   }
 }
