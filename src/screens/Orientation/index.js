e4d30817af6941936641dc85c433dc9e14b9f137
Yagho Mattos <mattosyagho@gmail.com>
2022-01-04 22:51:11 -0300

    feature: firebase orientation

--- src/screens/Orientation/index.js
+++ src/screens/Orientation/index.js
@@ -1,47 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { View, FlatList, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/core';
-import { useParseQuery } from '@parse/react-native';
-import Parse from 'parse/react-native.js';
-
-import { Button } from '../../components/Button/index';
-
-import { List, Divider } from 'react-native-paper';
 import { Ionicons } from '@expo/vector-icons';
-
+import { useNavigation } from '@react-navigation/core';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
+import { Divider, List } from 'react-native-paper';
+import { Button } from '../../components/Button/index';
+import { database } from '../../services/firebase';
 import styles from './styles';
-
-const parseQuery = new Parse.Query('SelectOrientations');
-parseQuery.descending('createdAt');
 
 export const Orientation = (props) => {
   const navigation = useNavigation();
 
   const [orientation, setOrientation] = useState('');
 
-  const examId = props.route.params[3].id;
-
+  const test = props.route.params.id;
   const propertys = props.route.params;
 
   useEffect(() => {
-    async function Search() {
-      var examPointer = {
-        __type: 'Pointer',
-        className: 'Exam',
-        objectId: examId,
-      };
-
-      parseQuery.equalTo('exam', examPointer);
-      const results = await parseQuery.find();
-
-      setOrientation(results);
-    }
-
-    Search();
-  }, [orientation]);
-
-  const results = useParseQuery(parseQuery).results;
-  Parse.User._clearCache();
+    var li = [];
+    database.ref('selectOrientation').on('value', (snapshot) => {
+      snapshot.forEach((child) => {
+        if (child.val().test == test) {
+          selectTestList.push({
+            orientation: child.val().orientation,
+          });
+        }
+      });
+      setOrientation(li);
+    });
+  }, []);
 
   return (
     <>
