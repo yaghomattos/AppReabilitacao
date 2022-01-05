cf9940cd470b0d1533975dc0491d032b8cbae2ea
Yagho Mattos <mattosyagho@gmail.com>
2022-01-04 23:10:00 -0300

    feature: home participant state and route params

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StatusBar, Text, View } from 'react-native';
 import { Button } from '../../components/Button/index';
+import { readParticipant } from '../../components/CRUDs/Participant/index';
 import { Logout } from '../../components/Logout/index';
 import styles from './styles';
 
 export function Home(props) {
-  const participant = props.route.params;
+  const cpf = props.route.params;
+
+  const [participant, setParticipant] = useState('');
+
+  useEffect(() => {
+    readParticipant(cpf).then((response) => {
+      setParticipant(response);
+    });
+  }, []);
 
   return (
     <>
@@ -14,32 +23,30 @@ export function Home(props) {
         <View style={styles.header}>
           <Text style={styles.header_text}>
             {'Bem Vindo - '}
-            <Text style={styles.header_text_bold}>
-              {participant.get('name')}
-            </Text>
+            <Text style={styles.header_text_bold}>{participant.name}</Text>
           </Text>
         </View>
         <View style={styles.background}>
           <Button
             title="Treinamento"
             onPress="ListSelectExercises"
-            props={participant.id}
+            props={participant}
           />
           <Button
             title="Avaliação"
             onPress="ListSelectTest"
-            props={participant.id}
+            props={participant}
           />
-          <Button title="Chat" onPress="Chat" props={participant.id} />
+          <Button title="Chat" onPress="Chat" props={participant.key} />
           <Button
             title="Monitoramento"
             onPress="Monitoring"
-            props={participant.id}
+            props={participant.key}
           />
           <Button
             title="Educacional"
             onPress="Educational"
-            props={participant.id}
+            props={participant.key}
           />
         </View>
         <Logout />
