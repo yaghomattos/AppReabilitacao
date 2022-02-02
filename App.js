916fe3ef5da9f5485ef02b0d98cc7e4ffc565080
Yagho Mattos <mattosyagho@gmail.com>
2022-02-02 01:42:11 -0300

    feature: provider timer context in App

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'react-native-gesture-handler';
+import { TimerContext } from './src/context/timer';
 import { AppRoutes } from './src/routes/app.routes';
 import './src/services/firebase';
 
 const App = () => {
-  return <AppRoutes />;
+  const [seconds, setSeconds] = useState(0);
+
+  return (
+    <TimerContext.Provider value={{ seconds, setSeconds }}>
+      <AppRoutes />
+    </TimerContext.Provider>
+  );
 };
 
 export default App;
