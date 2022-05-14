6a8885c8e7b9215e2d649002fb3a70f8a827640b
Yagho Mattos <mattosyagho@gmail.com>
2022-05-14 10:28:38 -0300

    feature: custom interval in context

--- App.js
+++ App.js
@@ -8,10 +8,20 @@ import './src/services/firebase';
 const App = () => {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
+  const [customInterval, setCustomInterval] = useState('');
   const [isSignedIn, setSignedIn] = useState(false);
 
   return (
-    <TimerContext.Provider value={{ minutes, setMinutes, seconds, setSeconds }}>
+    <TimerContext.Provider
+      value={{
+        minutes,
+        setMinutes,
+        seconds,
+        setSeconds,
+        customInterval,
+        setCustomInterval,
+      }}
+    >
       <AuthContext.Provider value={{ isSignedIn, setSignedIn }}>
         {!isSignedIn ? <AuthRouter /> : <FlowRouter />}
       </AuthContext.Provider>
